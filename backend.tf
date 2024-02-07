afe069685144117f180420a1455ed15d2b507295
Marcelo Arguello <marcelo.arguello@gmail.com>
2024-02-07 11:43:15 -0300

    Added a backend for the tf file on AWS S3

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "s3" {
-        profile = "terraform-elrond"
-        key = "terraform_deployer/tfstate.tfstate"
-        bucket = "marcelo-tftate-backup-deployer-2024"
+        profile = "terraform"
+        key = "terraform_clusterlab1/tfstate.tfstate"
+        bucket = "marcelo-tftate-backup-clusterlab1-2024"
         region = "us-east-1"
     }
 }
