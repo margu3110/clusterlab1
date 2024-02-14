9af013d7a839c9ccf0341bd0e3a2fbab47f0244a
Marcelo Arguello <marcelo.arguello@gmail.com>
2024-02-14 12:24:05 -0300

    variables not allowed on backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         profile = "terraform"
         key = "terraform_clusterlab1/tfstate.tfstate"
-        bucket = var.s3_backend[var.account]
+        bucket = "marcelo-tftate-backup-clusterlabisildur-2024"
         region = "us-east-1"
     }
 }
