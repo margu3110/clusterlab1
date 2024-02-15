35701c2797b2e716128a19d7a266581dc38f224a
Marcelo Arguello <marcelo.arguello@gmail.com>
2024-02-15 10:53:15 -0300

    Fixing backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         profile = "terraform"
         key = "terraform_clusterlab1/tfstate.tfstate"
-        bucket = "marcelo-tftate-backup-clusterlab1-2024"
+        bucket = "marcelo-tfstate-backend-clusterlabisildur-2024"
         region = "us-east-1"
     }
 }
